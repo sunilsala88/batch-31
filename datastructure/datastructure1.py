4c938914af9efe7c9e0c72d7efe8515f22bbb961
sunilsala88 <sunilsala88@gmail.com>
2025-03-23 18:44:14 +0530

    mar-23-1

--- datastructure/datastructure1.py
+++ datastructure/datastructure1.py
@@ -1,1 +1,21 @@
-print('hello world')
+
+l1=[22,33,44,55,66]
+print(l1)
+print(l1[:2])
+
+l1.append(66)
+l1.append(99)
+print(l1)
+l1.insert(2,58)
+print(l1)
+
+l1.remove(66)
+print(l1)
+l1.pop(0)
+print(l1)
+
+del l1[-1]
+print(l1)
+
+l1[0]=44
+print(l1)
