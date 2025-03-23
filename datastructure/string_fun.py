52d317ae07366fa0eecba56a8b17ea49eb15f767
sunilsala88 <sunilsala88@gmail.com>
2025-03-23 19:29:50 +0530

    mar23-7

---
+++ datastructure/string_fun.py
@@ -0,0 +1,15 @@
+s1='fessorpro  '
+print(s1.replace('e','o'))
+print(s1.index('o'))
+print(s1.upper())
+print(s1.capitalize())
+print(s1.strip())
+
+#2 function
+#split
+s2='tsla,goog,amzn,nvda'
+print(s2.split(','))
+#join
+
+l1=['h','e','l','l','o']
+print(''.join(l1))
