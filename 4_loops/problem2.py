2a6455eee2ced0c80180b6b138d2ec519f37e36e
sunilsala88 <sunilsala88@gmail.com>
2025-04-05 19:06:54 +0530

    apr-5-2

--- 4_loops/problem2.py
+++ 4_loops/problem2.py
@@ -3,22 +3,26 @@ stock_prices={'amzn': 555, 'tsla': 333, 'ongc': 667, 'reliance': 345, 'goog': 67
 for i,j in stock_prices.items():
     print(i,":",j)
 
-# portfolio=[]
+portfolio=[]
 
-# while True:
-#     stock_name=input('enter the stock name (press q to quit)')
-#     print(stock_name)
+while True:
+    stock_name=input('enter the stock name (press q to quit)')
+    print(stock_name)
 
-#     if stock_name.upper()=='Q':
-#         break
+    if stock_name.upper()=='Q':
+        break
 
-#     found=stock_prices.get(stock_name)
-#     if found:
-#         portfolio.append(stock_name)
-#     else:
-#         print('stock does not exist')
+    if stock_name=='ongc':
+        print('we cannot trade this stock')
+        continue
 
-# print(portfolio)
+    found=stock_prices.get(stock_name)
+    if found:
+        portfolio.append(stock_name)
+    else:
+        print('stock does not exist')
+
+print(portfolio)
 
 
 money=1000
@@ -35,7 +39,7 @@ while True:
 print(years)
 
 list1=[44,55,66,7,5]
-string1='hello'
+
 new_list=[]
 index1=-1
 
@@ -48,3 +52,18 @@ while True:
         break
     index1=index1-1
 print(new_list)
+
+string1='hello'
+
+new_string=""
+index1=-1
+
+while True:
+
+    print(index1)
+    new_string=new_string+string1[index1]
+
+    if index1==len(list1)*(-1):
+        break
+    index1=index1-1
+print(new_string)
