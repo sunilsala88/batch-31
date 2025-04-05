a1aca6e193383c0413ef12e7b3de75bc04877d0b
sunilsala88 <sunilsala88@gmail.com>
2025-04-05 18:55:59 +0530

    apr-5-1

---
+++ 4_loops/problem2.py
@@ -0,0 +1,50 @@
+stock_prices={'amzn': 555, 'tsla': 333, 'ongc': 667, 'reliance': 345, 'goog': 678}
+
+for i,j in stock_prices.items():
+    print(i,":",j)
+
+# portfolio=[]
+
+# while True:
+#     stock_name=input('enter the stock name (press q to quit)')
+#     print(stock_name)
+
+#     if stock_name.upper()=='Q':
+#         break
+
+#     found=stock_prices.get(stock_name)
+#     if found:
+#         portfolio.append(stock_name)
+#     else:
+#         print('stock does not exist')
+
+# print(portfolio)
+
+
+money=1000
+initial_money=money
+annual_ret=5
+years=0
+
+while True:
+    if money>2*initial_money:
+        break
+    money=money+(money*(annual_ret/100))
+    years=years+1
+
+print(years)
+
+list1=[44,55,66,7,5]
+string1='hello'
+new_list=[]
+index1=-1
+
+while True:
+
+    print(index1)
+    new_list.append(list1[index1])
+
+    if index1==len(list1)*(-1):
+        break
+    index1=index1-1
+print(new_list)
