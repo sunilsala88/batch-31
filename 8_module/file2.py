0ec1763e19bb3cbc0c9c8e56fecd2bdc79b7e5d6
sunilsala88 <sunilsala88@gmail.com>
2025-04-20 18:57:52 +0530

    apr-20-3

--- 8_module/file2.py
+++ 8_module/file2.py
@@ -10,7 +10,6 @@
 
 # import file1 as f1
 
-
 # print(f1.a)
 # print(f1.get_number())
 
@@ -19,3 +18,27 @@
 
 from file1 import get_number
 print(get_number())
+
+import new.file3 as f3
+print(f3.b)
+
+
+#module package library
+
+import random
+import time
+import os
+import sys
+
+import datetime
+print(datetime.datetime.now())
+
+print(os.getcwd())
+
+num=13
+if num==12:
+    sys.exit()
+
+print(random.randint(100,200))
+time.sleep(1)
+print(time.time())
