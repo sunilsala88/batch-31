9f3a146c2f148bdb450a32a1af1e9c2589a8c786
sunilsala88 <sunilsala88@gmail.com>
2025-04-27 18:36:02 +0530

    apr-27

--- 8_module/file2.py
+++ 8_module/file2.py
@@ -8,37 +8,46 @@
 
 
 
-import file1 as f1
+# import file1 as f1
 
-print(f1.a)
-print(f1.get_number())
+# print(f1.a)
+# print(f1.get_number())
 
-b1=f1.Broker('jay',123,1000)
+# b1=f1.Broker('jay',123,1000)
 
 
-from file1 import get_number
-print(get_number())
+# from file1 import get_number
+# print(get_number())
 
-import new.file3 as f3
-print(f3.b)
+# import new.file3 as f3
+# print(f3.b)
+
+
+# #module package library
+
+# import random
+# import time
+# import os
+# import sys
+
+# import datetime
+# print(datetime.datetime.now())
+
+# print(os.getcwd())
+
+# num=13
+# if num==12:
+#     sys.exit()
+
+# print(random.randint(100,200))
+# time.sleep(1)
+# print(time.time())
+
+#pip install package_name
+#pip freeze/list
+#pip unintall package_name
+#pip install package_name==2.2.0
+#pip install -U package_name
 
 
 #module package library
-
-import random
-import time
-import os
-import sys
-
-import datetime
-print(datetime.datetime.now())
-
-print(os.getcwd())
-
-num=13
-if num==12:
-    sys.exit()
-
-print(random.randint(100,200))
-time.sleep(1)
-print(time.time())
---
+++ 9_data_analysis/numpy1.py
@@ -0,0 +1,3 @@
+import numpy as np
+r1=np.array([1,2,3])
+print(r1)
