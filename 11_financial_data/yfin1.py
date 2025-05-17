422a2d01d2cbafbbf7fa933a139cbbf9f1436674
sunilsala88 <sunilsala88@gmail.com>
2025-05-17 19:47:30 +0530

    may-17-5

--- 11_financial_data/yfin1.py
+++ 11_financial_data/yfin1.py
@@ -3,13 +3,37 @@
 
 #pip install yfinance
 import yfinance as yf
-data=yf.download('TSLA',multi_level_index=False)
-print(data)
+# data=yf.download('TSLA',multi_level_index=False)
+# print(data)
 # print(data.columns)
 # l1=[]
 # for i in data.columns:
 #     l1.append(i[0])
 # data.columns=l1
 # print(data)
-d=yf.download(['TSLA','AMZN'],multi_level_index=False)
-print(d)
+# d=yf.download(['TSLA','AMZN'],multi_level_index=False)
+# print(d)
+
+
+# data=yf.download('TSLA',multi_level_index=False,period='5mo')
+# print(data)
+
+# data=yf.download('TSLA',multi_level_index=False,start='2024-01-01',end='2024-12-30')
+# print(data)
+
+# import datetime as dt
+# s=dt.datetime(2023,1,1)
+# e=dt.datetime(2023,12,30)
+# data=yf.download('TSLA',multi_level_index=False,start=s,end=e)
+# print(data)
+
+# data=yf.download('BTC-USD',multi_level_index=False,start='2024-01-01',end='2024-12-30',interval='30m')
+# print(data)
+
+
+data=yf.download('TSLA',multi_level_index=False,period='7d',interval='1m')
+print(data)
+data=yf.download('RELIANCE.NS',multi_level_index=False,period='7d',interval='1m')
+print(data)
+data=yf.download('RELIANCE.NS',multi_level_index=False,period='7d',interval='1m',ignore_tz=True)
+print(data)
