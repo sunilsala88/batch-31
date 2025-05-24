c04249f50efcac428a7eb08425102815dcd6f372
sunilsala88 <sunilsala88@gmail.com>
2025-05-24 19:36:50 +0530

    may24-5

--- 13_technical/tech1.py
+++ 13_technical/tech1.py
@@ -2,5 +2,13 @@ import yfinance as yf
 import pandas as pd
 
 stock_name = 'TSLA'
-df = yf.download(stock_name)
+df = yf.download(stock_name,multi_level_index=False,period='12mo')
 print(df)
+
+import mplfinance as mpf
+import pandas_ta as ta
+ema1=ta.ema(df['Close'])
+df['sma']=ta.sma(df['Close'])
+a=mpf.make_addplot(ema1,color='black')
+b=mpf.make_addplot(df['sma'],color='blue')
+mpf.plot(df,type='candle',style='yahoo',addplot=[a,b])
