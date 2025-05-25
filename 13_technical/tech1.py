293e7e1e16200a46db360ca74800dbe877f98902
sunilsala88 <sunilsala88@gmail.com>
2025-05-25 18:40:21 +0530

    may25-6

--- 13_technical/tech1.py
+++ 13_technical/tech1.py
@@ -7,8 +7,13 @@ print(df)
 
 import mplfinance as mpf
 import pandas_ta as ta
-ema1=ta.ema(df['Close'])
-df['sma']=ta.sma(df['Close'])
+# ema1=ta.ema(df['Close'])
+# df['sma']=ta.sma(df['Close'])
+import talib as ta1
+ema1=ta1.EMA(df['Close'])
+df['sma']=ta1.SMA(df['Close'])
+
+
 a=mpf.make_addplot(ema1,color='black')
 b=mpf.make_addplot(df['sma'],color='blue')
 mpf.plot(df,type='candle',style='yahoo',addplot=[a,b])
