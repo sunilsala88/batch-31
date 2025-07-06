18e393868d6efb39ffea663158267a4c7a519467
sunilsala88 <sunilsala88@gmail.com>
2025-07-06 19:08:45 +0530

    jul-6-4

---
+++ 15_backtesting/12_orb.py
@@ -0,0 +1,7 @@
+# 15-min TF
+# 9.15 - 9.30
+
+# 3 or 5 min candles closing above range of first 15-min candle
+
+# Apply - 9/21/50 moving average logic -- if 9>21>50 go for long
+# Close - if price is closing below Supertrend (10,1.5)
